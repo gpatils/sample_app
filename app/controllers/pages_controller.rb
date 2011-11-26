aa584ba18f084e6b644c59b6943e3d08061d7920
gpatils <gpatils@gmail.com>
2011-11-26 14:13:55 +0530

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,12 @@
 class PagesController < ApplicationController
   def home
+      @title = "Ruby Home"
   end
 
   def contact
+     @title ="ContactUs"
   end
-
+  def about
+    @title ="AboutUs"
+  end
 end
